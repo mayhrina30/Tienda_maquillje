982a96aff00387680a70ae8c0e6bb03766eb33b9
mayra manzaneda <urielortega300@gmail.com>
2023-11-05 23:22:49 -0300

    cambios
    seguimos probando

---
+++ db/basededatos.js
@@ -0,0 +1,7 @@
+const pool = createPool({
+  host: 'Localhost',
+  port: 9000,
+  user: 'root',
+  password: 'root',
+  database: 'clientes',
+});
--- src/app/models/product.model.ts
+++
@@ -1,6 +0,0 @@
- export interface Product{
-    id: Number;
-    title: string;
-    price: number;
-    
-}
